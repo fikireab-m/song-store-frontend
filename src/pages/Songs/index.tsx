54aba78cacac451dfc9d1a20fc4836aecbd7241e
fikireab-m <fikireab.meku@gmail.com>
2024-02-21 09:07:32 +0300

    _

--- src/pages/Songs/index.tsx
+++ src/pages/Songs/index.tsx
@@ -43,14 +43,14 @@ const Songs = () => {
                     <TableTitleText>
                         All Songs
                     </TableTitleText>
-                    <SearchForm >
+                    {/* <SearchForm >
                         <button>
                             <svg width="17" height="16" fill="none" xmlns="http://www.w3.org/2000/svg" role="img">
                                 <path d="M7.667 12.667A5.333 5.333 0 107.667 2a5.333 5.333 0 000 10.667zM14.334 14l-2.9-2.9" stroke="currentColor" strokeWidth="1.333" strokeLinecap="round" strokeLinejoin="round"></path>
                             </svg>
                         </button>
                         <input placeholder="Search here ..." required type="text" />
-                    </SearchForm>
+                    </SearchForm> */}
                 </TitleContainer>
                 <Table>
                     <thead>
